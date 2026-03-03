67b27a040ccd0a3e6723090324d377dc672a5a58
Ayyappa <ayyappareddy2005@gmail.com>
2026-03-04 00:50:08 +0530

    Updated Java code by collaborator

--- fgh/src/main/java/fgh/sum.java
+++ fgh/src/main/java/fgh/sum.java
@@ -5,7 +5,7 @@ public class sum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 50;
-		int b = 60;
+		int b = 20;
 		int sum = a+b;
 		System.out.print(sum);
 
