136c824a94cf950aff8751282c2588675fb6f654
Ayyappa <ayyappareddy2005@gmail.com>
2026-03-04 00:15:37 +0530

    initial

---
+++ fgh/src/main/java/fgh/sum.java
@@ -0,0 +1,14 @@
+package fgh;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 50;
+		int b = 60;
+		int sum = a+b;
+		System.out.print(sum);
+
+	}
+
+}
